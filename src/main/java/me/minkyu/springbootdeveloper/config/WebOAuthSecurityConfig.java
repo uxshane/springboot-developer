a5eb263706f4197220c1ed5c1208be71f8efc0ad
Min Kyu Kim <uxchampaign@gmail.com>
2024-11-11 13:03:55 +0900

    OAuth하나 securityconfig에 셋팅하는데 엄청난 시간이 소요되는 듯...

--- src/main/java/me/minkyu/springbootdeveloper/config/WebOAuthSecurityConfig.java
+++ src/main/java/me/minkyu/springbootdeveloper/config/WebOAuthSecurityConfig.java
@@ -10,15 +10,19 @@ import me.minkyu.springbootdeveloper.service.UserService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
 @Configuration
@@ -39,7 +43,7 @@ public class WebOAuthSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //토큰 방식을 사용할거라서 기존에 사용하던 폼로그인, 세션 비활성황
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable);
@@ -57,14 +61,27 @@ public class WebOAuthSecurityConfig {
         http.oauth2Login(
                 login -> login.loginPage("/login")
                         .authorizationEndpoint( authorization -> authorization
+                                //Authorization 요청과 관련된 상태 저장
                                 .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
                         ).successHandler(oAuthSuccessHandler())
-        ));
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserCustomService)));
+
+        http.logout(logout -> logout.logoutSuccessUrl("/login"));
+
+        http.exceptionHandling(exception -> exception
+                .defaultAuthenticationEntryPointFor(
+                          new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED), new AntPathRequestMatcher("/api/**")));
+
+        return http.build();
     }
 
     @Bean
     public OAuth2SuccessHandler oAuthSuccessHandler() {
-        return new OAuth2SuccessHandler();
+        return new OAuth2SuccessHandler(
+                tokenProvider, refreshTokenRepository, oAuth2AuthorizationRequestBasedOnCookieRepository(),
+                userService
+        );
     }
 
     @Bean
@@ -75,6 +92,11 @@ public class WebOAuthSecurityConfig {
     @Bean
     public TokenAuthenticationFilter tokenAuthenticationFilter() {
         return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/me/minkyu/springbootdeveloper/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/me/minkyu/springbootdeveloper/config/oauth/OAuth2SuccessHandler.java
@@ -1,6 +1,17 @@
 package me.minkyu.springbootdeveloper.config.oauth;
 
+import me.minkyu.springbootdeveloper.config.jwt.TokenProvider;
+import me.minkyu.springbootdeveloper.repository.RefreshTokenRepository;
+import me.minkyu.springbootdeveloper.service.UserService;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    public OAuth2SuccessHandler(TokenProvider tokenProvider,
+                                RefreshTokenRepository refreshTokenRepository,
+                                OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository,
+                                UserService userService) {
+
+
+
+    }
 }
